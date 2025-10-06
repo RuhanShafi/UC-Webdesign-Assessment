39c58b659d28250726cba9db8b590b10b0136a1c
Ruhan Shafi <imruhans@gmail.com>
2025-10-06 22:36:13 +1100

    First Commit

---
+++ Models/ErrorViewModel.cs
@@ -0,0 +1,8 @@
+namespace UC_Web_Assessment.Models;
+
+public class ErrorViewModel
+{
+    public string? RequestId { get; set; }
+
+    public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+}
